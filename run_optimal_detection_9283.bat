0511fb28b7d3f6dc5d46729f705f81838efc7eb9
amatov <6277757+amatov@users.noreply.github.com>
2023-10-13 19:33:50 -0700

    Add files via upload

---
+++ run_optimal_detection_9283.bat
@@ -0,0 +1,11 @@
+REM You can make a copy of this file and change
+REM the name of the INI file below to make is easy
+REM to run for a different configuration
+
+@pushd %~dp0
+
+optimal_ctc.exe datasets_optimal_9283.ini
+
+@echo.
+@pause
+@popd
